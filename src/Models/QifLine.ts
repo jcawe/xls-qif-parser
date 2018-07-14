efffad549719b8d4485f5c3982ff84caeb958efd
Jose Antonio Cáceres Romero <awelojc@gmail.com>
2018-07-14 14:03:25 +0200

    [CodeFactor] Apply fixes (#24)

--- src/Models/QifLine.ts
+++ src/Models/QifLine.ts
@@ -3,14 +3,14 @@ import { QifDetail } from "./QifDetail";
 export class QifLine {
     public details: QifDetail[] = [];
 
-    public constructor(line?: any){
+    public constructor(line?: any) {
         line = line || {};
 
-        if(line.details){
-            line.details.forEach(detail => {
+        if (line.details) {
+            line.details.forEach((detail) => {
                 const newDetail = new QifDetail(detail);
                 this.details.push(newDetail);
-            })
+            });
         }
     }
     public toString = (): string => {
